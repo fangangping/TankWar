477aa8142635e142e2f163f531f50ce69e7cb47f
fangangping <906370564@qq.com>
2017-02-18 23:51:20 +0800

    Test Git

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+/**
+ * Created by fangangping on 17/2/18.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+
+}
